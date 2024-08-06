f99eb02d6540a1f6bcf697e4adf7032020cc9522
gnkim827 <nagix999@gmail.com>
2024-08-06 17:10:45 +0900

    add images ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,27 @@ import { useCallback, useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-    const [images, setImages] = useState([]);
+    const COLS = 10;
+
+    const [imageSet, setImageSet] = useState([]);
     const [selectedImage, setSelectedImage] = useState({});
 
-    const fetchImages = useCallback(() => {}, []);
+    const fetchImages = useCallback(async () => {
+        const response = await axios.get("http://localhost:8000/images");
+        const imageDataset = [];
+        if (response.status === 200) {
+            let imageData = response.data;
+            while (imageData.length) {
+                imageDataset.push(imageData.splice(0, COLS));
+            }
+            setImageSet(imageDataset);
+        }
+    }, []);
+
+    console.log(selectedImage);
 
     useEffect(() => {
         fetchImages();
@@ -15,8 +30,31 @@ function App() {
 
     return (
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <button>추론</button>
+            </div>
+            <div className="">
+                <table>
+                    {imageSet.map((images) => {
+                        return (
+                            <tr>
+                                {images.map((image) => (
+                                    <td>
+                                        <img
+                                            onClick={(event) =>
+                                                setSelectedImage(image)
+                                            }
+                                            id={image.fileName}
+                                            alt={image.fileName}
+                                            src={`data:image/png;base64, ${image.base64}`}
+                                        />
+                                    </td>
+                                ))}
+                            </tr>
+                        );
+                    })}
+                </table>
+            </div>
         </div>
     );
 }
